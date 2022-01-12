a1c2e4d601c0d376a3d42f1d3024afbe33c11a05
AB <ab.vk20@gmail.com>
2022-01-11 23:03:55 -0500

    Array Usage in Go

--- booking-app/main.go
+++ booking-app/main.go
@@ -9,6 +9,9 @@ func main() {
 	var conferenceName = "GO Con" // or conferenceName := "Go Con" , In this case Golang automatically understands and it is called syntacticSugar in programming.
 	//Note this cannot be done in the below case of constants. and also if the syntacticSugar i.e. := is used the explicit mention of "int" or "uint" cannot be used
 	const conferenceTickets = 50
+
+	var booking = [50]string{"hope"}
+
 	fmt.Println("-------Checking Variable Types-------")
 	fmt.Printf("Type for conferenceTickers is %T, for remainingTickets is %T, for conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 	fmt.Println("--------------")
@@ -20,9 +23,21 @@ func main() {
 
 	var userName string
 	var userTickets int
+	var userEmail string
+	//Taking User Inputs
+	fmt.Printf("Enter your name")
+	fmt.Scan(&userName)
+	fmt.Printf("Enter your email\n")
+	fmt.Scan(&userEmail)
+	fmt.Printf("Enter number of Tickets to be booked\n")
+	fmt.Scan(&userTickets)
 
-	userName = "Tom"
-	userTickets = 2
+	remainingTickets = remainingTickets - userTickets
+	booking[6] = userName
 
-	fmt.Printf("User %v booked %v tickets for the show.\n", userName, userTickets)
+	fmt.Printf("Thank You %v for booking %v tickets for the show.\n You'll recevie a confirmation email on %v\n", userName, userTickets, userEmail)
+	fmt.Printf("First Element %v", booking[0])
+	fmt.Printf("Booking Array %v\n", booking)
+	fmt.Printf("Booking is of Type %T\n", booking)
+	fmt.Printf("Array Length is %v\n", len(booking))
 }
